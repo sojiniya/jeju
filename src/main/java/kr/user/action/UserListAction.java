592b0a23e18174b6f37de8d3be69e49b22f34bfe
soyeon_lim <lsy96060@naver.com>
2022-02-07 17:22:12 +0900

    관리자 전용 수정

--- src/main/java/kr/user/action/UserListAction.java
+++ src/main/java/kr/user/action/UserListAction.java
@@ -1,10 +1,15 @@
 package kr.user.action;
 
+import java.util.List;
+
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import javax.servlet.http.HttpSession;
 
 import kr.controller.Action;
+import kr.user.dao.UserDAO;
+import kr.user.vo.UserVO;
+import kr.util.PagingUtil;
 
 public class UserListAction implements Action{
 
@@ -13,8 +18,36 @@ public class UserListAction implements Action{
 
 		HttpSession session = request.getSession();
 		Integer user_num =(Integer)session.getAttribute("user_num");
+		if(user_num==null) {
+			return "redirect:/user/loginForm.do";
+		}
+		Integer user_auth = (Integer)session.getAttribute("user_auth");
+		if(user_auth<3) {
+			return "/WEB-INF/views/common/notice.jsp";
+		}
+		//관리자로 로그인한 경우
+		String pageNum = request.getParameter("pageNum");
+		if(pageNum==null) {
+			pageNum="1";
+		}
+		String keyfield=request.getParameter("keyfield");
+		String keyword = request.getParameter("keyword");
 		
-		return null;
+		UserDAO dao =UserDAO.getInstance();
+		int count = dao.getUserByAdmin(keyfield, keyword);
+		
+		PagingUtil page = new PagingUtil(keyfield,keyword,Integer.parseInt(pageNum),count,10,10,"userList.do");
+		
+		List<UserVO> list = null;
+		if(count>0) {
+			list = dao.getListUserByAdmin(keyfield,keyword,page.getStartCount(), page.getEndCount());
+		}
+		
+		request.setAttribute("count", count);
+		request.setAttribute("list", list);
+		request.setAttribute("pagingHtml", page.getPagingHtml());
+		
+		return "/WEB-INF/views/member/userList.do";
 	}
 
 }

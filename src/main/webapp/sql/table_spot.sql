f742a97ef867a4f9d8fc9fa7598e736500dc3c75
SUJIN <cainstear@gmail.com>
2022-02-07 17:41:24 +0900

    추천 장소 테이블 sql문 수정

--- src/main/webapp/sql/table_spot.sql
+++ src/main/webapp/sql/table_spot.sql
@@ -1,7 +1,6 @@
-
 /* 추천 장소 테이블*/
 create table jboard_spot(
-  board_spot_num number not null,
+  spot_num number not null,
   title varchar2(150) not null,
   content clob not null,
   hit number(5) default 0 not null,
@@ -10,47 +9,40 @@ create table jboard_spot(
   filename varchar2(150),
   ip varchar2(40) not null,
   user_num number not null,
-  constraint jboard_spot_pk primary key (board_spot_num),
+  constraint jboard_spot_pk primary key (spot_num),
   constraint jboard_spot_fk foreign key (user_num) references juser (user_num)
 );
 
 create sequence jboard_spot_seq;
 
 /*추천 장소 좋아요*/
-create table jboard_spot_good(
-    board_spot_num number(10) not null,
+create table jgood_spot(
+    spot_num number(10) not null,
     user_num number(10) not null,
     good number(1) not null,
-    constraint jboard_spot_like_fk foreign key(board_spot_num) references jboard_spot(board_spot_num),
+    constraint jboard_spot_like_fk foreign key(spot_num) references jboard_spot(spot_num),
     constraint jboard_spot_like_fk2 foreign key(user_num) references juser(user_num)
 );
 
-create table jboard_spot_comment(
-  spot_comment_num number not null,
-  board_spot_num number not null,
+create table jcmt_spot(
+  spotcmt_num number not null,
+  spot_num number not null,
   content varchar2(300) not null,
   reg_date date default sysdate not null,
   modify_date date,
   user_num number not null,
-  constraint spot_comment_num_pk primary key (spot_comment_num),
-  constraint board_spot_num_fk foreign key (board_spot_num) references jboard_spot (board_spot_num),
-  constraint jboard_spot_comment_fk foreign key (user_num) references juser (user_num)
+  constraint spotcmt_num_pk primary key (spotcmt_num),
+  constraint spot_num_fk foreign key (spot_num) references jboard_spot (spot_num),
+  constraint jcmt_spot_fk foreign key (user_num) references juser (user_num)
 );
 
-create sequence jcomment_spot_seq;
+create sequence jcmt_spot_seq;
 
 /*추천 장소 코멘트 좋아요*/
-create table jboard_spot_comment_good(
-    spot_comment_num number(10) not null,
+create table jcmtgood_spot(
+    spotcmt_num number(10) not null,
     user_num number(10) not null,
     good number(1) not null,
-    constraint jboard_spot_comment_like_fk foreign key(spot_comment_num) references jboard_spot_comment(spot_comment_num),
-    constraint jboard_spot_comment_like_fk2 foreign key(user_num) references juser(user_num)
+    constraint jcmt_spot_like_fk foreign key(spotcmt_num) references jcmt_spot(spotcmt_num),
+    constraint jcmt_spot_like_fk2 foreign key(user_num) references juser(user_num)
 );
-
-
-
-
-
-
-

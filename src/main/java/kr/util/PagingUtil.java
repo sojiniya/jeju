940a04c786cb2840e4efdbab097fd7191166a341
soyeon_lim <lsy96060@naver.com>
2022-02-07 16:43:01 +0900

    pagingutil 바꾸기

--- src/main/java/kr/util/PagingUtil.java
+++ src/main/java/kr/util/PagingUtil.java
@@ -1,34 +1,37 @@
 package kr.util;
 
 public class PagingUtil {
-	private int startCount; // 한 페이지에서 보여줄 게시글의 시작 번호
-	private int endCount; // 한 페이지에서 보여줄 게시글의 끝 번호
+	private int startCount;	 // 한 페이지에서 보여줄 게시글의 시작 번호
+	private int endCount;	 // 한 페이지에서 보여줄 게시글의 끝 번호
 	private StringBuffer pagingHtml;// 페이지 표시 문자열
 
 	/**
-	 * currentPage : 현재페이지 totalCount : 전체 게시물 수 rowCount : 한 페이지의 게시물의 수 pageCount
-	 * : 한 화면에 보여줄 페이지 수 pageUrl : 호출 페이지 url addKey : 부가적인 key 없을 때는 null 처리
-	 * (&num=23형식으로 전달할 것)
-	 */
-	public PagingUtil(int currentPage, int totalCount, int rowCount, int pageCount, String pageUrl) {
-		this(null, null, currentPage, totalCount, rowCount, pageCount, pageUrl, null);
+	 * currentPage : 현재페이지
+	 * totalCount : 전체 게시물 수
+	 * rowCount : 한 페이지의  게시물의 수
+	 * pageCount : 한 화면에 보여줄 페이지 수
+	 * pageUrl : 호출 페이지 url
+	 * addKey : 부가적인 key 없을 때는 null 처리 (&num=23형식으로 전달할 것)
+	 * */
+	public PagingUtil(int currentPage, int totalCount, int rowCount,
+			int pageCount, String pageUrl) {
+		this(null,null,currentPage,totalCount,rowCount,pageCount,pageUrl,null);
 	}
-
-	public PagingUtil(int currentPage, int totalCount, int rowCount, int pageCount, String pageUrl, String addKey) {
-		this(null, null, currentPage, totalCount, rowCount, pageCount, pageUrl, addKey);
+	public PagingUtil(int currentPage, int totalCount, int rowCount,
+			int pageCount, String pageUrl, String addKey) {
+		this(null,null,currentPage,totalCount,rowCount,pageCount,pageUrl,addKey);
 	}
-
-	public PagingUtil(String keyfield, String keyword, int currentPage, int totalCount, int rowCount, int pageCount,
-			String pageUrl) {
-		this(keyfield, keyword, currentPage, totalCount, rowCount, pageCount, pageUrl, null);
+	public PagingUtil(String keyfield, String keyword, int currentPage, int totalCount, int rowCount,
+			int pageCount,String pageUrl) {
+		this(keyfield,keyword,currentPage,totalCount,rowCount,pageCount,pageUrl,null);
 	}
-
-	public PagingUtil(String keyfield, String keyword, int currentPage, int totalCount, int rowCount, int pageCount,
-			String pageUrl, String addKey) {
-
-		if (addKey == null)
-			addKey = ""; // 부가키가 null 일때 ""처리
-
+	public PagingUtil(String keyfield, String keyword, int currentPage, int totalCount, int rowCount,
+			int pageCount,String pageUrl,String addKey) {
+		
+		String sub_url = "";
+		if(keyword != null) sub_url = "&keyfield="+keyfield+"&keyword="+keyword;
+		if(addKey != null) sub_url += addKey;
+		
 		// 전체 페이지 수
 		int totalPage = (int) Math.ceil((double) totalCount / rowCount);
 		if (totalPage == 0) {
@@ -51,63 +54,42 @@ public class PagingUtil {
 		// 이전 block 페이지
 		pagingHtml = new StringBuffer();
 		if (currentPage > pageCount) {
-			if (keyword == null) {// 검색 미사용시
-				pagingHtml.append("<a href=" + pageUrl + "?pageNum=" + (startPage - 1) + addKey + ">");
-			} else {
-				pagingHtml.append("<a href=" + pageUrl + "?keyfield=" + keyfield + "&keyword=" + keyword + "&pageNum="
-						+ (startPage - 1) + addKey + ">");
-			}
-			pagingHtml.append("이전");
+			pagingHtml.append("<a href="+pageUrl+"?pageNum="+ (startPage - 1) + sub_url +">");
+			pagingHtml.append("[이전]");
 			pagingHtml.append("</a>");
 		}
-		pagingHtml.append("&nbsp;|&nbsp;");
-		// 페이지 번호.현재 페이지는 빨간색으로 강조하고 링크를 제거.
+		//페이지 번호.현재 페이지는 빨간색으로 강조하고 링크를 제거.
 		for (int i = startPage; i <= endPage; i++) {
 			if (i > totalPage) {
 				break;
 			}
 			if (i == currentPage) {
-				pagingHtml.append("&nbsp;<b> <font color='#FE9A2E'>");
+				pagingHtml.append("&nbsp;<b><span style='color:red;'>");
 				pagingHtml.append(i);
-				pagingHtml.append("</font></b>");
+				pagingHtml.append("</span></b>");
 			} else {
-				if (keyword == null) {// 검색 미사용시
-					pagingHtml.append("&nbsp;<a href='" + pageUrl + "?pageNum=");
-				} else {
-					pagingHtml.append("&nbsp;<a href='" + pageUrl + "?keyfield=" + keyfield + "&keyword=" + keyword
-							+ "&pageNum=");
-				}
+				pagingHtml.append("&nbsp;<a href='"+pageUrl+"?pageNum=");
 				pagingHtml.append(i);
-				pagingHtml.append(addKey + "'>");
+				pagingHtml.append(sub_url+"'>");
 				pagingHtml.append(i);
 				pagingHtml.append("</a>");
 			}
 			pagingHtml.append("&nbsp;");
 		}
-		pagingHtml.append("&nbsp;&nbsp;|&nbsp;&nbsp;");
 		// 다음 block 페이지
 		if (totalPage - startPage >= pageCount) {
-			if (keyword == null) {// 검색 미사용시
-				pagingHtml.append("<a href=" + pageUrl + "?pageNum=" + (endPage + 1) + addKey + ">");
-			} else {
-				pagingHtml.append("<a href=" + pageUrl + "?keyfield=" + keyfield + "&keyword=" + keyword + "&pageNum="
-						+ (endPage + 1) + addKey + ">");
-			}
-			pagingHtml.append("다음");
+			pagingHtml.append("<a href="+pageUrl+"?pageNum="+ (endPage + 1) + sub_url +">");
+			pagingHtml.append("[다음]");
 			pagingHtml.append("</a>");
 		}
 	}
-
 	public StringBuffer getPagingHtml() {
 		return pagingHtml;
 	}
-
 	public int getStartCount() {
 		return startCount;
 	}
-
 	public int getEndCount() {
 		return endCount;
 	}
-
 }
